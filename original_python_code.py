0a9a4794c7c2745b0e84e24ca8dfd395fcc93122
swcarr1984 <swcarr1984@hotmail.com>
2023-07-18 20:21:36 +0800

    updated python code commit

--- original_python_code.py
+++ original_python_code.py
@@ -13,6 +13,29 @@ def myFunction(name,fav_number):
     
 myFunction('Scott', 27)
 
+class Engineer:
+    def __init__(self,name, type, years_of_experience):
+        self.skill = "problem solver"
+        self.name = name
+        self.type = type
+        self.years_of_experience = years_of_experience
+
+    def getName(self):
+        return self.name
+    def getType(self):
+        return self.type
+    def getskill(self):
+        return self.skill
+    def getyears_of_experience(self):
+        return self.years_of_experience
+
+eng1 = Engineer("Jim","Instrument",2)
+print(eng1.getName(),":",eng1.getskill(),":",eng1.getType(),":",eng1.getyears_of_experience())
+
+eng2 = Engineer("Johan","Computer",4)
+print(eng2.getName(),":",eng2.getskill(),":",eng2.getType(),":",eng2.getyears_of_experience())
+
+
 
 
 
